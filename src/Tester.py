be7582dfed9095bd929846afde3d4b07095ee672
Pierre <pierremaier@outlook.com>
2026-08-03 14:34:43 +0200

    updated Tester.py for merge

--- src/Tester.py
+++ src/Tester.py
@@ -1,4 +1,4 @@
-from src.ModelDeepening import ModelDeepening
+from src.model_deepening import ModelDeepening
 
 
 # class Tester is currently dummy class, serves as a shell for more specific Tester classes
